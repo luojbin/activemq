c313e7b48f09fdc33662348d12232400cc121d5e
luojbin <z1_2x@msn.com>
2019-12-12 16:15:22 +0800

    init

---
+++ boot/consumer/src/main/java/com/loyofo/activemq/boot/consumer/controller/JtConsumer.java
@@ -0,0 +1,39 @@
+package com.loyofo.activemq.boot.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+
+@RequestMapping("jt")
+@RestController
+public class JtConsumer {
+
+    @Autowired
+    private JmsTemplate template;
+
+    @GetMapping("get")
+    public String get() throws JMSException {
+        Message message = template.receive("demoQueue");
+        if (message instanceof TextMessage) {
+            String text = ((TextMessage) message).getText();
+            System.out.println("JmsTemplate 获取到消息头ID " + message.getJMSMessageID());
+            System.out.println("JmsTemplate 获取到消息内容 " + text);
+            return text;
+        }
+        return "获取消息错误";
+    }
+
+    @GetMapping("getMsg")
+    public String getMsg() {
+        Object message = template.receiveAndConvert("demoQueue");
+        System.out.println("JmsTemplate 获取到消息内容 " + message);
+        return (String) message;
+    }
+}
---
+++ boot/provider/src/main/java/com/loyofo/activemq/boot/provider/controller/JtProvider.java
@@ -0,0 +1,56 @@
+package com.loyofo.activemq.boot.provider.controller;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RequestMapping("jt")
+@RestController
+public class JtProvider {
+    @Autowired
+    private JmsTemplate messagingTemplate;
+
+    @PostMapping("sendDemo")
+    public String send(String message) {
+        messagingTemplate.convertAndSend("demoQueue", message);
+        return "手动消息 发送成功";
+    }
+
+    @PostMapping("sendStr")
+    public String sendStr(String message) {
+        messagingTemplate.convertAndSend("strQueue", message);
+        return "str 发送成功";
+    }
+    @PostMapping("sendMap")
+    public String sendMap(String message) {
+        Map<String, Object> map = new HashMap<>(16);
+        map.put("str", message);
+        map.put("integer", 123);
+        map.put("boolean", false);
+        messagingTemplate.convertAndSend("mapQueue", map);
+        return "map 发送成功";
+    }
+
+    @PostMapping("sendMapAndReply")
+    public String sendMapAndReply() {
+        messagingTemplate.convertAndSend("replyQueue", "来自 jt 的消息", message1 -> {
+            message1.setJMSReplyTo(new ActiveMQQueue("reply"));
+            return message1;
+        });
+        return "map 发送成功";
+    }
+
+    @PostMapping("sendDlq")
+    public String sendDlq(String message) {
+        messagingTemplate.convertAndSend("retry", message);
+        return "jt 消息重发与死信 发送成功";
+    }
+
+}
